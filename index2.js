881f079e1bff1da154e3dbea54cbd112c87503ff
Leonard Choo <leochoodev@gmail.com>
2021-09-03 13:16:05 +0900

    template working wtf

--- index2.js
+++ index2.js
@@ -6,9 +6,12 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
   to: "leochoodev@gmail.com", // Change to your recipient
   from: "leochootest@gmail.com", // Change to your verified sender
-  subject: "Sending with SendGrid is Fun",
-  text: "and easy to do anywhere, even with Node.js",
-  html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  templateId: "d-43903e12c5a241959a1f60bb52564a59",
+  dynamicTemplateData: {
+    subject: "Testing Templates",
+    name: "Some One",
+    city: "Denver",
+  },
 };
 
 sgMail
